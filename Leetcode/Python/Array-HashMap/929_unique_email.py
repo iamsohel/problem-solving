67ff79c93ef40a3ccabb6abfced0d3db6bdcfb69
Sohel Rana <sohelcuetcse11@gmail.com>
2024-02-26 10:50:24 +0600

    leetcode 929 solved

---
+++ Leetcode/Python/Array-HashMap/929_unique_email.py
@@ -0,0 +1,25 @@
+def numUniqueEmails(emails):
+    realEmails = []
+    for email in emails:
+        e = ""
+        splittedEmail = email.split("@")
+        for c in splittedEmail[0]:
+            if c == "+":
+                break
+            if c.isalnum():
+                e += c
+        print(e)
+        added = e + '@' + splittedEmail[1]
+        realEmails.append(added)
+    print(realEmails, set(realEmails))
+    return len(set(realEmails))
+
+
+print(numUniqueEmails(["test.email+alex@leetcode.com",
+      "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"]))
+
+
+for i in range(10):
+    if i == 2:
+        break
+    print(i)
