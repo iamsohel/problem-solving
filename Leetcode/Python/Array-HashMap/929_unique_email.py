8524e65546c74b0b77cb5c122012f8271bf7baa5
Sohel Rana <sohelcuetcse11@gmail.com>
2024-02-26 10:56:31 +0600

    leetcode 929 solved with o(n)

--- Leetcode/Python/Array-HashMap/929_unique_email.py
+++ Leetcode/Python/Array-HashMap/929_unique_email.py
@@ -15,7 +15,22 @@ def numUniqueEmails(emails):
     return len(set(realEmails))
 
 
-print(numUniqueEmails(["test.email+alex@leetcode.com",
+# print(numUniqueEmails(["test.email+alex@leetcode.com",
+#       "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"]))
+
+
+def numUniqueEmails2(emails):
+    unique = set()
+    for email in emails:
+        local, domain = email.split("@")
+        local = local.split("+")[0]
+        local = local.replace(".", "")
+        unique.add((local, domain))
+    print(unique)
+    return len(unique)
+
+
+print(numUniqueEmails2(["test.email+alex@leetcode.com",
       "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"]))
 
 
